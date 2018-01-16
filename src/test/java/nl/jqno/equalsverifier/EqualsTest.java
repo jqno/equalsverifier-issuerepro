33bd0b656dd522121babc4be255172961ef05823
Jan Ouwens <jan.ouwens@gmail.com>
2018-01-16 08:40:20 +0100

    Reproducing issue 183

--- src/test/java/nl/jqno/equalsverifier/EqualsTest.java
+++ src/test/java/nl/jqno/equalsverifier/EqualsTest.java
@@ -2,10 +2,13 @@ package nl.jqno.equalsverifier;
 
 import org.junit.Test;
 
+import java.awt.color.ColorSpace;
+
 public class EqualsTest {
     @Test
     public void testEqualsAndHashCode() {
         EqualsVerifier.forClass(MyClass.class)
+                .withPrefabValues(ColorSpace.class, ColorSpace.getInstance(ColorSpace.CS_sRGB), ColorSpace.getInstance(ColorSpace.CS_LINEAR_RGB))
                 .verify();
     }
 }
--- src/test/java/nl/jqno/equalsverifier/MyClass.java
+++ src/test/java/nl/jqno/equalsverifier/MyClass.java
@@ -1,5 +1,26 @@
 package nl.jqno.equalsverifier;
 
+import java.awt.color.ICC_Profile;
+import java.util.Objects;
+
 public class MyClass {
-    // Fill in the class that produces your issue here
+    private final ICC_Profile p;
+
+    public MyClass(ICC_Profile p) {
+        this.p = p;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof MyClass)) {
+            return false;
+        }
+        MyClass other = (MyClass)obj;
+        return Objects.equals(p, other.p);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(p);
+    }
 }
