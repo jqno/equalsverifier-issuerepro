abe8f4ef3b8929bbd01b39ad0652713f8a80973d
Jan Ouwens <jan.ouwens@gmail.com>
2019-09-05 16:48:12 +0200

    Implements equals and hashCode

--- src/main/java/x/MyData.java
+++ src/main/java/x/MyData.java
@@ -1,7 +1,9 @@
 package x;
 
-import lombok.Value;
+import java.util.Objects;
+
 import lombok.NonNull;
+import lombok.Value;
 
 @Value
 public final class MyData {
@@ -10,5 +12,19 @@ public final class MyData {
 
     public MyData(String s) {
         this.something = s;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof MyData)) {
+            return false;
+        }
+        MyData other = (MyData)obj;
+        return Objects.equals(something, other.something);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(something);
     }
 }
